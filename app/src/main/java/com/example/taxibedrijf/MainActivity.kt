8f5d63df870d99d5d7e3cd956acdabf50016023f
Lars Jarred Speetjens <lars@speetjens.net>
2023-03-06 12:35:33 +0100

    Main layout with search bar

---
+++ app/src/main/java/com/example/taxibedrijf/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.taxibedrijf
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val searchinput : TextView = findViewById(R.id.SearchInput)
+        val searchbutton : Button = findViewById(R.id.SearchBtn)
+
+        searchbutton.setOnClickListener {
+            println(searchinput.text)
+        }
+
+
+    }
+}
